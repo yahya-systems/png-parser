7c5cd8d98b498146c39392dabbf44b8d15dd8cb2
yahya-systems <yahyaaitahmed@icloud.com>
2026-03-01 20:12:55 +0000

    Delete build directory

--- build/objects/main.d
+++
@@ -1,2 +0,0 @@
-build/objects/./main.o: main.cpp src/image-processor.h
-src/image-processor.h:
--- build/objects/src/image-processor.d
+++
@@ -1,3 +0,0 @@
-build/objects/./src/image-processor.o: src/image-processor.cpp \
-  src/image-processor.h
-src/image-processor.h:
